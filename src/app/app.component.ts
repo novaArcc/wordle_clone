0fe73316b8b15a99f93c5cba8f0d4663f3b7e24f
Tim.Heller <Tim.Heller@cas.de>
2025-01-13 17:16:42 +0100

    Correctly show input fields and dont show result box if no results are there to show

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { EMPTY, map, Observable } from 'rxjs';
+import { WordleService } from './wordle.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent{
+
+  public constructor(private wordleService: WordleService){}
+
   title = 'wordle-clone';
+
+  showResults(): Observable<boolean> {
+    return this.wordleService.getListOfResults().pipe(map(r => r.length != 0));
+  }
+
 }
